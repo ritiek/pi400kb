ce9c18d8865b76d9d8de2ad1ba80569f84918fe6
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2023-07-14 22:50:36 +0530

    Reverse LED behaviour

--- hook.sh
+++ hook.sh
@@ -4,9 +4,9 @@
 echo none > /sys/class/leds/PWR/trigger
 case $1 in
     0) # Ungrabbed
-        echo 0 > /sys/class/leds/PWR/brightness
+        echo 1 > /sys/class/leds/PWR/brightness
         ;;
     1) # Grabbed
-        echo 1 > /sys/class/leds/PWR/brightness
+        echo 0 > /sys/class/leds/PWR/brightness
         ;;
 esac
