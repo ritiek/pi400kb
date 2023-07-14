38405cf4e2b36f18c592ae15dbb78f0ab0cd7095
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2023-07-14 22:50:14 +0530

    Make hooks.sh compatible with bullseye

--- hook.sh
+++ hook.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Basic example pi400kb hook
 # This script is given the argument 1 for grabbed, 0 for ungrabbed.
-echo none > /sys/class/leds/led0/trigger
+echo none > /sys/class/leds/PWR/trigger
 case $1 in
     0) # Ungrabbed
-        echo 0 > /sys/class/leds/led0/brightness
+        echo 0 > /sys/class/leds/PWR/brightness
         ;;
     1) # Grabbed
-        echo 1 > /sys/class/leds/led0/brightness
+        echo 1 > /sys/class/leds/PWR/brightness
         ;;
 esac
